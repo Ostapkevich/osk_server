daa307b7f6273d779f37962bc79b1c09c166fd72
unknown <ostapkevich.sergej@gmail.com>
2023-12-22 20:41:40 +0200

    Ver 1.2 прокат

--- src/materials/materials.controller.ts
+++ src/materials/materials.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, Body } from '@nestjs/common';
+import { Controller, Get, Param, Body, Query, Post, Put } from '@nestjs/common';
 import { AppService } from 'src/app.service';
+import { IaddRolled } from './dto/matarials';
 
 @Controller('/materials')
 export class MaterialsController {
@@ -10,7 +11,7 @@ export class MaterialsController {
     try {
       const rolled_type = 'SELECT idrolled_type, name_typerolled, ind FROM rolled_type;';
       const steels = 'SELECT idsteel, steel, ind FROM steels';
-      const rolleds = 'SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel ORDER BY idrolled_type, steels.ind, d, t ;'
+      const rolleds = 'SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel ORDER BY idrolled_type, d, t ;'
       const data = await this.appService.query(rolled_type, steels, rolleds);
       return { rolled_type: data[0][0], steels: data[1][0], rolleds: data[2][0] };
     } catch (error) {
@@ -20,60 +21,98 @@ export class MaterialsController {
 
   @Get('rolleds/:rolledtype/:steel/:position')
   async loadRolled(@Param('rolledtype') rolledtype: number, @Param('steel') steel: number, @Param('position') position: number,
-    @Body() bodyData: string[]) {
+    @Query() bodyData) {
     try {
-      let str = ``;
-      if (bodyData && bodyData.length > 0) {
-
-        for (const i of bodyData) {
-          str = str + `name_rolled regexp regexp ('${i}') AND `;
-        }
-        str = str.slice(0, str.length - 4);
+        let str = ``;
+        if (bodyData) {
+          let i = 0;
+          for (const key in bodyData) {
+            if (Object.prototype.hasOwnProperty.call(bodyData, key)) {
+              const value = bodyData[key];
+              str = str + `name_rolled regexp ('${value}') AND `;
+            }
+          }
+          str = str.slice(0, str.length - 4);
+     
       }
-      console.log(str);
       let sql: string;
-      switch (rolledtype) {
-        case 1:
-          switch (steel) {
-            case 1: //любой тип проката и любая сталь
-              if (str.length > 0) {
-                sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE '${str}' ORDER BY idrolled_type, steels.ind, d, t LIMIT ${position},20;`
-              } else {
-                sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel ORDER BY idrolled_type, steels.ind, d, t LIMIT ${position},20;`
-              }
-              break;
-            default: //любой тип проката и определенная сталь
-              if (str.length > 0) {
-                sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.idsteel=${steel} ORDER BY idrolled_type, steels.ind, d, t LIMIT ${position},20;`
-              } else {
-                sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.idsteel=${steel} AND '${str}' ORDER BY idrolled_type, steels.ind, d, t LIMIT ${position},20;`
-              }
-              break;
+        if (+rolledtype === 1) {
+        if (+steel === 1) {
+          if (str.length > 0) {
+            sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE ${str} ORDER BY idrolled_type, d, t LIMIT ${position},20;`
+          } else {
+            sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel ORDER BY idrolled_type, d, t LIMIT ${position},20;`
           }
-          break;
-        default: //определенный тип проката и любая сталь
-          switch (steel) {
-            case 1:
-              if (str.length > 0) {
-                sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.id_rolled=${rolledtype} ORDER BY idrolled_type, steels.ind, d, t LIMIT ${position},20;`
-              } else {
-                sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.id_rolled=${rolledtype} AND '${str}' ORDER BY idrolled_type, steels.ind, d, t LIMIT ${position},20;`
-              } break;
-            default: //определенный тип проката и определенная сталь
-              if (str.length > 0) {
-                sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.id_rolled=${rolledtype} AND rolled.idsteel=${steel} ORDER BY idrolled_type, steels.ind, d, t LIMIT ${position},20;`
-              } else {
-                sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.id_rolled=${rolledtype} AND rolled.idsteel=${steel} AND '${str}' ORDER BY idrolled_type, steels.ind, d, t LIMIT ${position},20;`
-
-              } break;
+        } else {
+          if (str.length > 0) {
+            sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.idsteel=${steel} AND ${str} ORDER BY idrolled_type, d, t LIMIT ${position},20;`
+          } else {
+            sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.idsteel=${steel}  ORDER BY idrolled_type, d, t LIMIT ${position},20;`
           }
-          break;
+        }
+      } else {
+        if (+steel === 1) {
+          if (str.length > 0) {
+            sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.idrolled_type=${rolledtype} AND ${str} ORDER BY idrolled_type, d, t LIMIT ${position},20;`
+          } else {
+            sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.idrolled_type=${rolledtype}  ORDER BY idrolled_type, d, t LIMIT ${position},20;`
+          }
+        } else {
+          if (str.length > 0) {
+           sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.idrolled_type=${rolledtype} AND rolled.idsteel=${steel}AND ${str} ORDER BY idrolled_type, d, t LIMIT ${position},20;`
+          } else {
+          sql = `SELECT id_rolled, name_rolled, d, t, steels.steel, weight FROM rolled JOIN steels ON rolled.idsteel=steels.idsteel WHERE rolled.idrolled_type=${rolledtype} AND rolled.idsteel=${steel}  ORDER BY idrolled_type, d, t LIMIT ${position},20;`
+          }
+        }
       }
       const rolledsData = await this.appService.query(sql);
-
       return { rolleds: rolledsData[0][0] };
     } catch (error) {
       return { serverError: error.message };
+    }
+  }
+
+  @Post('addRolled')
+  async createRolled(@Body() bodyData) {
+    try {
+      let strInsertData = '';
+      let arrData: Array<any>=[];
+      for (const key in bodyData) {
+         if (Object.prototype.hasOwnProperty.call(bodyData, key)) {
+         let param=bodyData[key];
+         arrData.push(param);
+        }
+      }
+      strInsertData = `INSERT rolled (idrolled_type, idsteel, name_rolled, d, weight, t) VALUES (?,?,?,?,?,?)`;
+      console.log(arrData)
+      const insertMain = await this.appService.execute(strInsertData,arrData);
+      if (insertMain[0]['affectedRows'] === 1) {
+        return { response: 'ok' };
+      }
+    } catch (error) {
+       return { serverError: 'Ошибка сервера: ' + error.message };
+    }
+  }
+
+  @Put('updateRolled')
+  async updateRolled(@Body() bodyData) {
+    try {
+      let arrData: Array<any>=[];
+      for (const key in bodyData) {
+         if (Object.prototype.hasOwnProperty.call(bodyData, key)) {
+         let param=bodyData[key];
+           arrData.push(param);
+        }
+      }
+      const strUpdateData = `UPDATE rolled SET name_rolled=?, d=?, weight=?, t=? WHERE id_rolled=?`;
+      console.log(strUpdateData)
+      const data = await this.appService.execute(strUpdateData,arrData);
+      
+      if (data[0]['affectedRows'] === 1) {
+        return { response: 'ok' };
+      }
+    } catch (error) {
+       return { serverError: 'Ошибка сервера: ' + error.message };
     }
   }
 
